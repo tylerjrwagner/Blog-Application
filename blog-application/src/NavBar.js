98e03a8fb8f0afb2fd293b645c0231d3e2259ae7
tylerjrwagner <123418013+tylerjrwagner@users.noreply.github.com>
2023-02-12 17:27:49 -0500

    Add files via upload
    First Commit for Application

---
+++ blog-application/src/NavBar.js
@@ -0,0 +1,34 @@
+import { Link, useNavigate } from 'react-router-dom';
+import useUser from './Hooks/useUser';
+import { getAuth, signOut } from 'firebase/auth';
+
+const NavBar = () => {
+    const { user } = useUser();
+    const navigate = useNavigate();
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/articles">Articles</Link>
+                </li>
+            </ul>
+            <div className="nav-right">
+                { user
+                ? <button onClick={() => {
+                    signOut(getAuth());
+                }}>Log Out</button>
+                : <button onClick={() => {
+                    navigate("/login");
+                }}>Log In</button>}
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
---
+++ blog-application/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0vobChmBWk3lCUqLWmhlBTLPfbSYnJrQ",
+  authDomain: "react-blog-6c0ec.firebaseapp.com",
+  projectId: "react-blog-6c0ec",
+  storageBucket: "react-blog-6c0ec.appspot.com",
+  messagingSenderId: "688299281626",
+  appId: "1:688299281626:web:90bbef5f71c9dcf2e369e3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
