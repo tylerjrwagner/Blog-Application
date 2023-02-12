35ad910ca3c32acd437eaa7b71054721e1fb9c96
tylerjrwagner <123418013+tylerjrwagner@users.noreply.github.com>
2023-02-12 17:24:27 -0500

    Add files via upload
    First Commit for Application Backend

---
+++ blog-backend/src/db.js
@@ -0,0 +1,14 @@
+import { MongoClient } from 'mongodb';
+let db;
+
+async function connectToDb(cb) {
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.batqx2k.mongodb.net/?retryWrites=true&w=majority`);
+    await client.connect()
+    db = client.db("react-blog-db");
+    cb();
+}
+
+export {
+    db,
+    connectToDb
+};
